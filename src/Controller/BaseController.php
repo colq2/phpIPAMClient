66016a7bbe2a1abb14251a682329d4b0a14af7da
Oliver Wycisk <wycisk@fh-aachen.de>
2018-01-07 13:52:21 +0100

    Filled Base Controller with functions

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -12,20 +12,57 @@ use function PhpIPAMClient\phpipamConnection;
 
 abstract class BaseController
 {
-	protected $name;
 
-	public static function getStatic()
+	protected static $name;
+
+	public function get(array $identifier = array(), array $params = array())
 	{
-
+		return self::getStatic($identifier, $params);
 	}
 
-	public function get(array $identifer = array(), array $params = array())
+	public static function getStatic(array $identifier = array(), array $params = array())
 	{
-		phpipamConnection()->call('get', $this->name, $identifer, $params);
+		return phpipamConnection()->call('get', self::name, $identifier, $params);
 	}
 
-	public function post()
+	public function post(array $identifier = array(), array $params = array())
 	{
-
+		return self::postStatic($identifier, $params);
 	}
+
+	public static function postStatic(array $identifier = array(), array $params = array())
+	{
+		return phpipamConnection()->call('post', self::name, $identifier, $params);
+	}
+
+	public function put(array $identifier = array(), array $params = array())
+	{
+		return self::putStatic($identifier, $params);
+	}
+
+	public static function putStatic(array $identifier = array(), array $params = array())
+	{
+		return phpipamConnection()->call('put', self::name, $identifier, $params);
+	}
+
+	public function patch(array $identifier = array(), array $params = array())
+	{
+		return self::putStatic($identifier, $params);
+	}
+
+	public function patchStatic(array $identifier = array(), array $params = array())
+	{
+		return self::putStatic($identifier, $params);
+	}
+
+	public function delete(array $identifier = array(), array $params = array())
+	{
+		return self::deleteStatic($identifier, $params);
+	}
+
+	public function deleteStatic(array $identifier = array(), array $params = array())
+	{
+		return phpipamConnection()->call('delete', self::name, $identifier, $params);
+	}
+
 }
